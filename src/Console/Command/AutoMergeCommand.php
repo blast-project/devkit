3f1f1ec12f6f189c49a898924461cb98539827d9
GlennCavarle <glenn.cavarle@gmail.com>
2017-04-18 16:11:00 +0200

    fixed command pr-auto-merge

--- src/Console/Command/AutoMergeCommand.php
+++ src/Console/Command/AutoMergeCommand.php
@@ -29,8 +29,8 @@ class AutoMergeCommand extends AbstractCommand
         parent::configure();
 
         $this
-            ->setName('auto-merge')
-            ->setDescription('Merges branches of repositories if there is no conflict.')
+            ->setName('pr-auto-merge')
+            ->setDescription('Merges pull requests if there is no conflict.')
             ->addArgument('bundles', InputArgument::IS_ARRAY, 'To limit the dispatcher on given bundles(s).', array());
     }
 
@@ -86,29 +86,55 @@ class AutoMergeCommand extends AbstractCommand
      */
     private function executeMergeBranches($owner, $repoName, array $repoConfig)
     {
-
         $this->io->title($owner . '/' . $repoName);
 
+        $base = 'master';
+        $head = 'BlastCI:' . static::DEVKIT_BRANCH;
+        $sha = null;
+        $id = null;
+
+        //find the pull request
+        $this->io->comment('Finding pull request...');
+        $pulls = $this->githubClient->pullRequests()
+            ->all($owner, $repoName, array(
+            'state' => 'open',
+            'head' => 'master'
+        ));
+
+        foreach ($pulls as $pull) {
+            if ($pull['head']['label'] == $head &&
+                strpos($pull['title'], 'DevKit updates') !== false) {
+                $id = $pull['number'];
+                $sha = $pull['head']['sha'];
+                $this->io->comment(sprintf('Found pull request id[%s] title[%s] sha[%s]', $id, $pull['title'], $sha));
+                $this->mergePullRequest($owner, $repoName, $id, $head, $base, $sha);
+                return;
+            }
+        }
+    }
+
+    /**
+     * 
+     * @throws \Blast\DevKit\Console\Command\RuntimeException
+     */
+    private function mergePullRequest($owner, $repoName, $id, $head, $base, $sha)
+    {
         if (!$this->apply) {
             return;
         }
 
-        $base = 'master';
-        $head = static::DEVKIT_BRANCH;
-
         try {
             // Merge message should be removed when following PR will be merged and tagged.
             // https://github.com/KnpLabs/php-github-api/pull/379
-            $response = $this->githubClient->repo()->merge(
-                $owner, $repoName, $base, $head, sprintf('Merge %s into %s', $head, $base)
+            $response = $this->githubClient->pullRequests()->merge(
+                $owner, $repoName, $id, sprintf('Merge %s into %s', $head, $base), $sha
             );
-
             if (is_array($response) && array_key_exists('sha', $response)) {
                 $this->io->success('Merged ' . $head . ' into ' . $base);
             } else {
                 $this->io->comment('Nothing to merge on ' . $base);
             }
-        } catch (RuntimeException $e) {
+        } catch (\RuntimeException $e) {
             if (409 === $e->getCode()) {
                 $this->io->warning('Merging of ' . $head . ' into ' . $base . ' contains conflicts. Skipped.');
                 return;
